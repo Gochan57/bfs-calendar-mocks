1be0a12db54b00551e892c68e6cc6e122e73f63c
SBT-Chmutin-GN <GNChmutin.SBT@sberbank.ru>
2018-06-26 14:28:44 +0300

    Сервис check-headers для отображения всех хэдеров запроса

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const port = 8080;
+const port = 8082;
 const router = express.Router();
 
 router.post('/task-service-bh/v4.0/config', (req: express.Request, res: express.Response) => {
@@ -56,6 +56,22 @@ router.post('/task-service-bh/v4.0/tasks/delete/:taskid', (req: express.Request,
             success: true
         })
     }, 2000)
+})
+
+router.get('/check-headers', (req: express.Request, res: express.Response) => {
+    console.log('GET Request headers: ', req.headers);
+    console.log('GET Request cookies: ', req.cookies);
+    res.json({
+        success: true
+    })
+})
+
+router.post('/check-headers', (req: express.Request, res: express.Response) => {
+    console.log('POST Request headers: ', req.headers);
+    console.log('POST Request cookies: ', req.cookies);
+    res.json({
+        success: true
+    })
 })
 
 app.use('/', router);
